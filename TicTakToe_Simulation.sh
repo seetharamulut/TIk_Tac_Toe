56b323d52632cc2d43d42507396301e91538e833
Seetharamulu T <thumurugoti.seetharamulu@gmail.com>
2020-06-12 07:55:26 +0530

    added start

---
+++ TicTakToe_Simulation.sh
@@ -0,0 +1,16 @@
+#! /bin/bash/ 
+
+echo "Welcome to TicTakToe game"
+
+echo "Starting a new game"
+
+array=(. . . . . . . . .)
+
+function printBoard(){
+
+	echo "0   ${array[0]} ${array[1]} ${array[2]}"
+	echo "1   ${array[3]} ${array[4]} ${array[5]}"
+	echo "2   ${array[6]} ${array[7]} ${array[8]}"
+}
+
+printBoard
